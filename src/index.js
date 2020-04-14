9af61e132f8c062b5beb4524a265fb6965e2d14b
Trey Pendragon <tpendragon@princeton.edu>
2020-04-14 14:54:03 -0700

    Stuff.

--- src/index.js
+++ src/index.js
@@ -9,25 +9,28 @@ const IIIF = require('iiif-processor');
 const middy = require('middy');
 const { cors, httpHeaderNormalizer } = require('middy/middlewares');
 
-const handleRequest = (event, context, callback) => {
+const handleRequest = async (event, context) => {
   try {
-    new IIIFLambda(event, context, callback, process.env.tiffBucket)
-      .processRequest();
+    console.log("Handling request.")
+    let result = await new IIIFLambda(event, context, process.env.tiffBucket).processRequest();
+    console.log(result);
+    return result;
   } catch (err) {
-    callback(err, null);
+    console.log(err);
+    return err;
   }
 };
 
 class IIIFLambda {
-  constructor (event, context, callback, sourceBucket) {
+  constructor (event, context, sourceBucket) {
     this.event = event;
     this.context = context;
-    this.respond = callback;
     this.sourceBucket = sourceBucket;
     this.handled = false;
   }
 
   directResponse (result) {
+    console.log("Sending response.");
     var base64 = /^image\//.test(result.contentType);
     var content = base64 ? result.body.toString('base64') : result.body;
     var response = {
@@ -39,24 +42,28 @@ class IIIFLambda {
       isBase64Encoded: base64,
       body: content
     };
-    this.respond(null, response);
+    return response;
   }
 
   handleError (err, _resource) {
+    console.log("Got error.");
     if (err.statusCode) {
-      this.respond(null, {
+      console.log(err);
+      return {
         statusCode: err.statusCode,
         headers: { 'Content-Type': 'text/plain' },
         body: 'Not Found'
-      });
+      };
     } else if (err instanceof this.resource.errorClass) {
-      this.respond(null, {
+      console.log(err);
+      return {
         statusCode: 400,
         headers: { 'Content-Type': 'text/plain' },
         body: err.toString()
-      });
+      };
     } else {
-      this.respond(err, null);
+      console.log(err);
+      return;
     }
   }
 
@@ -85,49 +92,51 @@ class IIIFLambda {
   }
 
   checkForOptionsRequest() {
-    if (this.handled) return this;
+    console.log("Checking for options request");
+    if (this.handled) return;
+    console.log("Not handled.")
     if (this.event.httpMethod === 'OPTIONS') {
-      this.respond(null, { statusCode: 204, body: null });
+      console.log("Options Request Detected");
       this.handled = true;
+      return { statusCode: 204, body: null }
     }
-    return this;
   }
 
   checkForInfoJsonRedirect() {
-    if (this.handled) return this;
+    console.log("Checking for Info JSON Redirect");
+    if (this.handled) return;
+    console.log("Not handled.");
     if (this.fileMissing()) {
+      console.log("File missing, redirecting.");
       var location = this.eventPath() + '/info.json';
-      this.respond(null, { statusCode: 302, headers: { 'Location': location }, body: "Redirecting to info.json" });
-      this.handled = true;
+      this.handled = true
+      return { statusCode: 302, headers: { 'Location': location }, body: "Redirecting to info.json" };
     }
-    return this;
   }
 
-  execute() {
-    if (this.handled) return this;
+  async execute() {
+    console.log("Executing.");
+    if (this.handled) return;
 
     var scheme = this.event.headers['X-Forwarded-Proto'] || 'http';
     var host = this.event.headers['X-Forwarded-Host'] || this.event.headers['Host'];
     var uri = `${scheme}://${host}${this.eventPath()}`;
+    console.log(uri)
 
     this.resource = new IIIF.Processor(uri, id => this.s3Object(id));
-
-    this.resource
-      .execute()
-      .then(result => this.directResponse(result))
-      .catch(err => this.handleError(err));
-
-    this.handled = true;
-    return this;
+    console.log("Got resource");
+    this.handled = true
+    console.log("Fired Resource Execution.");
+    return this.resource
+          .execute()
+          .then(result => this.directResponse(result))
+          .catch(err => this.handleError(err));
   }
 
-  processRequest () {
+  async processRequest () {
     AWS.config.region = this.context.invokedFunctionArn.match(/^arn:aws:lambda:(\w+-\w+-\d+):/)[1];
 
-    this
-      .checkForOptionsRequest()
-      .checkForInfoJsonRedirect()
-      .execute()
+    return this.checkForOptionsRequest() || this.checkForInfoJsonRedirect() || this.execute()
   }
 
   s3Object (id) {
