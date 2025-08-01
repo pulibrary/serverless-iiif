db80b5adb65609d8d1d30b429e22e0a3ba82c0ca
Eliot Jordan <eliotj@princeton.edu>
2025-08-01 11:59:41 -0500

    work on edge

--- src_edge/index.js
+++ src_edge/index.js
@@ -1,10 +1,9 @@
 'use strict';
 
 const AWS = require('aws-sdk');
-const getCacheBucket = () => process.env.cacheBucket;
-const getCached = (key) => {
+const getCached = (bucket, key) => {
   const s3 = new AWS.S3();
-  s3.headObject({ Bucket: getCacheBucket(), Key: key }, (err) => {
+  s3.headObject({ Bucket: bucket, Key: key }, (err) => {
     if (err) {
       return false;
     } else {
@@ -15,25 +14,36 @@ const getCached = (key) => {
 
 module.exports.viewerRequest = (event, context, callback) => {
   const request = event.Records[0].cf.request;
+  const cacheBucket = request.headers['iiif-cache-bucket'];
   const key = new URL(request.uri).pathname.replace(/^\//, '');
-  const cached = getCached(key);
+  const cached = getCached(cacheBucket, key);
 
   if (cached) {
+    // const response = {
+    //   status: 404, // Use 404 to force CloudFront to fail over to the cache
+    //   isBase64Encoded: false,
+    //   body: ''
+    // };
+
     const response = {
-      status: 404, // Use 404 to force CloudFront to fail over to the cache
+      status: 200,
       isBase64Encoded: false,
-      body: ''
+      body: 'Item is cached'
     };
 
     callback(null, response);
   } else {
-    // Stash the original URI
-    request.headers["x-original-uri"] = [ { "key": "x-original-uri", "value": request.uri } ]
-    // The lambda will write to an S3 bucket and fall back to it if the image is
-    // too big. Unfortunately the object URL for S3 replaces % with %25, so
-    // fallback doesn't work. So rewrite % to %25, and then the lambda will use
-    // x-original-uri, while S3 will have the right request URI.
-    request.uri = request.uri.replace(/%/g, "%25")
-    callback(null, request);
+    // // Stash the original URI
+    // request.headers["x-original-uri"] = [ { "key": "x-original-uri", "value": request.uri } ]
+    // request.uri = request.uri.replace(/%/g, "%25")
+    // callback(null, request);
+
+    const response = {
+      status: 200,
+      isBase64Encoded: false,
+      body: 'Item is NOT cached'
+    };
+
+    callback(null, response);
   }
 };
