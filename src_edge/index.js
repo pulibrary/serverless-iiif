63186ee52bd7e29df53816b9e81d5442e5af3d8f
Eliot Jordan <eliot.jordan@gmail.com>
2026-01-29 10:24:19 -0600

    Handle libimages uris with loris/iiif

--- src_edge/index.js
+++ src_edge/index.js
@@ -2,11 +2,20 @@
 
 module.exports.viewerRequest = (event, context, callback) => {
   const request = event.Records[0].cf.request;
-  const uri = request.uri;
 
-  // Check if the request URI matches loris, loris2, or path has .jp2 extension
-  if (/loris2?\//.test(uri) || /\.jp2/.test(uri)) {
-    request.uri = uri.replace(/loris2?\//g, 'iiif/2/').replace(/\.jp2/g, '');
+  // Check if the request URI matches loris/iiif
+  if (/loris\/iiif/.test(request.uri)) {
+    request.uri = request.uri.replace(/loris\//g, '');
+  }
+
+  // Check if the request URI matches loris, loris2
+  if (/loris2?\//.test(request.uri)) {
+    request.uri = request.uri.replace(/loris2?\//g, 'iiif/2/');
+  }
+
+  // Check if the request URI has .jp2 extension
+  if (/\.jp2/.test(request.uri)) {
+    request.uri = request.uri.replace(/\.jp2/g, '');
   }
   callback(null, request);
 };
